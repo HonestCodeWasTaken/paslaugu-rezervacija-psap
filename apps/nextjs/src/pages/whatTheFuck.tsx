f0c999985d49a813bdb77b170e44c4a6cabb2107
honestdev <honestdev@yahoo.com>
2023-05-08 02:11:01 +0300

    what the fuck

---
+++ apps/nextjs/src/pages/whatTheFuck.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CreateBusinessModal from "components/Modals/CreateBusinessModal";
+
+type Props = {};
+
+const whatTheFuck = (props: Props) => {
+  return (
+    <div className="w-full h-full items-center justify-center">
+      <CreateBusinessModal />
+    </div>
+  );
+};
+
+export default whatTheFuck;
