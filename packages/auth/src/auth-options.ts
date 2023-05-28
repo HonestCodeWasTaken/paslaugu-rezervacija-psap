508ec7cb735c1e5c818bf2dcd6a66b766065d1ae
honestdev <honestdev@yahoo.com>
2023-05-28 19:33:02 +0300

    roles

--- packages/auth/src/auth-options.ts
+++ packages/auth/src/auth-options.ts
@@ -6,27 +6,6 @@ import GoogleProvider from "next-auth/providers/google";
 import { User, prisma } from "@acme/db";
 
 /**
- * Module augmentation for `next-auth` types
- * Allows us to add custom properties to the `session` object
- * and keep type safety
- * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
- **/
-declare module "next-auth" {
-  interface Session extends DefaultSession {
-    user: {
-      id: string;
-      // ...other properties
-      // role: UserRole;
-    } & DefaultSession["user"];
-  }
-
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
-}
-
-/**
  * Options for NextAuth.js used to configure
  * adapters, providers, callbacks, etc.
  * @see https://next-auth.js.org/configuration/options
